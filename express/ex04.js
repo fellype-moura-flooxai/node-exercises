9644a693f935ebaaae0f7ec41a37411ffac9a5da
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-05-09 09:46:58 -0300

    feat: ex04 route method

---
+++ express/ex04.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const server = express()
+
+server.route('/clientes')
+.get((req, res) => res.send('lista de clientes'))
+.post((req, res) => res.send('novo cliente'))
+.put((req, res) => res.send('altera cliente'))
+.delete((req, res) => res.send('remove cliente'))
+
+server.listen(3000, () => console.log('executando...'))
